c716996bd280f9ee90b61d92a954a4495b23cddc
Mai Nguyen <mai.ng.nguyen02@gmail.com>
2023-01-22 22:27:06 +0200

    Reading input file

---
+++ cnc.py
@@ -0,0 +1,26 @@
+'''
+Assignment: G-Code interpreter 
+User: Mai Nguyen 
+Description: 
+Implement a program that reads the attached G-Code program from a file and parses the instructions. 
+The program is expected to call MachineClient's stub methods to simulate execution. 
+MachineClient implements a simplified interface to control a CNC machine.
+'''
+
+import sys
+
+
+#inputfile
+inFile = sys.argv[1]
+with open(inFile, 'r') as f:
+    gcode = f.read()
+
+print(gcode)
+
+
+def getLine():
+
+def MachineClass: 
+
+def main():
+
