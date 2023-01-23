ba6b3b8c060ac5385e29248a222f9829f523b9a9
Mai Nguyen <mai.ng.nguyen02@gmail.com>
2023-01-23 17:55:55 +0200

    Implementing MachineClient class and editing

--- cnc.py
+++ cnc.py
@@ -15,8 +15,8 @@ inFile = sys.argv[1]
 with open(inFile, 'r') as f:
     gcode_Lines = f.readlines()
 
-prorgammStatus = False 
-cmdDict = {}
+programmStatus = False 
+
 
 def filtering(gcode_line):
     '''
@@ -36,8 +36,13 @@ def filtering(gcode_line):
     else:
         return 3
 
-def startingProgramm(flag = True):
-    prorgammStatus = True
+def startingProgramm(flag = True, value = "Unnamed"):
+    programmStatus = True
+    print("Starting programm number : {}".format(value))
+
+def endingProrgamm():
+    programmStatus = False
+    print("End of programm")
 
 def commenting(cmt):
     cmt = cmt.replace('(', '')
@@ -56,7 +61,8 @@ def programmInfo(no):
 def preppingLine(line):
     cmd_s = line.split()
     index = int(cmd_s[0][1:])
-    cmdDict[index] = cmd_s[1:]
+    commands = cmd_s[1:]
+    return index, commands
 
 
 def getLine(gcode_line):
@@ -66,7 +72,77 @@ def getLine(gcode_line):
     '''
     return 
 
-#class MachineClient:
+class MachineClient:
+    def _init_(self, x_cor = 0, y_cor = 0, z_cor = 0, name= "Unnamed"):
+        self.name_prog = name
+        self.x_cor = x_cor
+        self.y_cor = y_cor
+        self.z_cor = z_cor
+        self.tool = None
+        self.feed_rate = 0
+        self.spindle_speed = 0
+        self.coolant_status = False
+
+    def home(self):
+        ''' 
+        Moving the machine back to home position
+        '''
+        self.x_cor, self.y_cor, self.z_cor = 0, 0, 0
+        print("Moving to home.")
+
+    def move(self, x, y, z):
+        '''
+        Move spindle to given coordinates
+        '''
+        self.x_cor, self.y_cor, self.z_cor = x, y, z
+        print("Moving to X={:.3f}, Y={:.3f}, Z={:.3f} [mm]".format(x, y, z))
+
+
+    def move_x(self, value):
+        '''
+        Move spindle to given X_coordinate. 
+        Unchanged Y_Coordinate and Z_Coordinate
+        '''
+        self.x_cor = value 
+        print("Moving X to {:.3f} [mm].".format(value))
+    
+    def move_y(self, value):
+        '''
+        Move spindle to given Y_coordinate. 
+        Unchanged X_Coordinate and Z_Coordinate
+        '''
+        self.y_cor = value 
+        print("Moving Y to {:.3f} [mm].".format(value))
+
+    def move_z(self, value):
+        '''
+        Move spindle to given Z_coordinate. 
+        Unchanged X_Coordinate and Y_Coordinate
+        '''
+        self.z_cor = value 
+        print("Moving Z to {:.3f} [mm].".format(value))
+    
+    def set_feed_rate(self, value):
+        self.feed_rate = value
+        print("Using feed rate {} [mm/s].".format(value))
+    
+    def set_spindle_speed(self, value):
+        self.spindle_speed = value
+        print("Using spindle speed {} [mm/s].".format(value))
+    
+    def set_tool_name(self, tool_name):
+        self.tool = tool_name
+
+    def change_tool(self, tool_name):
+        print("Changing tool '{:.s}'.".format(tool_name))
+    
+    def coolant_on(self):
+        self.coolant_status = True
+        print("Coolant turned on.")
+
+    def coolant_off(self):
+        self.coolant_status = False
+        print("Coolant turn off.")
 
 
 def main():
@@ -74,5 +150,19 @@ def main():
     for gcode_Line in gcode_Lines:
         type_ = filtering(gcode_Line)
         print(type_)
+        if type_ == 0:
+            pass
+        if type_ == 1:
+            commenting(gcode_Line)
+        if type_ == 2:
+            prog_num, gcode_num = programmInfo(gcode_Line)
+            startingProgramm(True, prog_num)
+        if type_ == 3: 
+            index, cmds = preppingLine(gcode_Line)
+            if index == "N1":
+                pass
+            else:
+                pass
+
 
 main()
